b41851a6a9c9ab33fdfdc5f4f617b904eddda84b
KrystylGP <krystylgeraldizo@gmail.com>
2024-10-18 21:52:54 +0200

    Update Product.cs

--- Assignment_Product_Catalog/Models/Product.cs
+++ Assignment_Product_Catalog/Models/Product.cs
@@ -3,13 +3,13 @@
 namespace Assignment_Product_Catalog.Models;
 
 public class Product
-{   //Fältar med produktens egenskaper.
+{ 
     public Guid Id { get; private set; }
     public string Name { get; set; } = null!;
     public string? Description { get; set; } = null!;
     public decimal? Price { get; set; } = null!;
     public int? StockQuantity { get; set; } = 0!;
-    public Product()
+    public Product() //Konstruktor
     {
         Id = Guid.NewGuid(); //Skapar automatiskt ett unikt ID för produkten.
     }
